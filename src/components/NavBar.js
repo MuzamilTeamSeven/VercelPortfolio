77a752e98bf63aca8722946d750b191736f11af0
Muzamil Team 7 <muzamil@teamseven.ltd>
2025-04-09 17:23:33 +0500

    Logo Updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,7 +35,10 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
+            <div className="text-white">
+              <div>Muzamil</div>
+              <div>Saleem</div>
+            </div>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
